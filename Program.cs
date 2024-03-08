b982fca281d1f82e963377437d2470b504a850d0
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-03-08 23:22:56 +0200

    changes added to the program class

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo { Title = "Movies API", Version = "v1" });
+});
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
